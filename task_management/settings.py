bc0c35762a574aa61266a60e0122a96dacd65559
Estiak Ahmed <mdasif49678@gmail.com>
2026-03-01 12:53:06 +0600

    postgresql database relation build

--- task_management/settings.py
+++ task_management/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task-management',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+
+class Employee(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+
 # Create your models here.
+class Task(models.Model):
+    project = models.ForeignKey("project", on_delete=models.CASCADE,default=1)
+    title = models.CharField(max_length=255)
+    description= models.TextField()
+    assign_to = models.ManyToManyField(Employee)
+    due_date = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    
+class TaskDetails(models.Model):
+    HIGH = 'H'
+    MEDIUM = 'M'
+    LOW = 'L'
+    PRIORITY_OPTIONS = (
+        (HIGH, 'High'),
+        (MEDIUM,'Medium'),
+        (LOW,'Low')
+    )
+    task = models.OneToOneField(Task, on_delete=models.CASCADE)
+    assign_to = models.CharField(max_length=100)
+    priority = models.CharField(max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
+    
+    
+class Project(models.Model):
+    name = models.CharField(max_length=250)
+    start_date = models.DateField()
